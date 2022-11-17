74dd509eda7876884ec6ca00e364282877cfe341
Igor Martins <igorjosemartins01@gmail.com>
2022-11-17 17:48:59 -0300

    update sms msg

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -34,7 +34,7 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = String.format("O vendedor %s foi destaque em %s com um total de R$ %.2f", sale.getSellerName(), date, sale.getAmount()); 
+		String msg = "Vendedor " + sale.getSellerName() + " foi destaque em " + date + " com um total de " + sale.getAmount(); 
 
 		Twilio.init(twilioSid, twilioKey);
 
